cfb42aa89e2a6536ab6cf8ff2d7f2a8f9918a363
Allen St Fort <allenstfort1@gmail.com>
2021-06-07 03:20:01 +0000

    Excuse Generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,43 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  for (let whoCounter = 0; whoCounter < who.length; whoCounter++) {
+    let randomWho = Math.floor(Math.random() * who.length);
+
+    for (
+      let actionCounter = 0;
+      actionCounter < action.length;
+      actionCounter++
+    ) {
+      let randomAction = Math.floor(Math.random() * action.length);
+
+      for (let whatCounter = 0; whatCounter < what.length; whatCounter++) {
+        let randomWhat = Math.floor(Math.random() * what.length);
+
+        for (let whenCounter = 0; whenCounter < when.length; whenCounter++) {
+          let randomWhen = Math.floor(Math.random() * when.length);
+
+          console.log(
+            who[randomWho] +
+              action[randomAction] +
+              what[randomWhat] +
+              when[randomWhen]
+          );
+        }
+      }
+    }
+  }
+  return;
 };
