c2b54d1745e78d9f216b88e928d5df99825a22bc
Allen St Fort <allenstfort1@gmail.com>
2021-06-11 00:50:47 +0000

    Fixed the generator

--- src/app.js
+++ src/app.js
@@ -19,31 +19,13 @@ window.onload = function() {
     "while I was praying"
   ];
 
-  for (let whoCounter = 0; whoCounter < who.length; whoCounter++) {
-    let randomWho = Math.floor(Math.random() * who.length);
+  let randomWho = Math.floor(Math.random() * who.length);
+  let randomAction = Math.floor(Math.random() * action.length);
+  let randomWhat = Math.floor(Math.random() * what.length);
+  let randomWhen = Math.floor(Math.random() * when.length);
 
-    for (
-      let actionCounter = 0;
-      actionCounter < action.length;
-      actionCounter++
-    ) {
-      let randomAction = Math.floor(Math.random() * action.length);
-
-      for (let whatCounter = 0; whatCounter < what.length; whatCounter++) {
-        let randomWhat = Math.floor(Math.random() * what.length);
-
-        for (let whenCounter = 0; whenCounter < when.length; whenCounter++) {
-          let randomWhen = Math.floor(Math.random() * when.length);
-
-          console.log(
-            who[randomWho] +
-              action[randomAction] +
-              what[randomWhat] +
-              when[randomWhen]
-          );
-        }
-      }
-    }
-  }
+  document.querySelector(
+    "#excuse"
+  ).innerHTML = `${who[randomWho]} ${action[randomAction]} ${what[randomWhat]} ${when[randomWhen]}`;
   return;
 };
